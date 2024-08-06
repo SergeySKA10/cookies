96f76cf4f0eb8db419cd77403a9efc898bdb50c1
Sergey <karakoskinsergej@gmail.com>
2024-08-06 16:15:35 +0300

    refactoring code

--- README.md
+++ README.md
@@ -1,42 +1,87 @@
-# Cookies module ES6
+# Class "cookies"
 Cookies for website
 
-This module is intended to implement cookies on a website page.
-The module is implemented in a native Java script, using the ES6 standard.
+EN:
+
+This module is intended to implement cookies on a website.
+The module is implemented in a native JavaScript, using the ES6 standard.
 
 USAGE:
 
-1.  Move the file "cookieConsent.js" to your project folder or copy the code from the "cookieConsent.js" file to your separate cookies file.
+1.  Move the file "cookieConsent.js" to your project folder.
 
 2.  Import using command 'import cookieConsent from ...' in your main file 'main.js'.
 
-3.  In the main file call the function cookieConsent({}) inside which, as a parameter, pass an objects with the following properties or an empty string as the default parameter('') :
+3.  In the main file call the function cookieConsent({}) inside which, as a parameter, pass an objects with the following properties:
 
 ```
 cookiesConsent(
-  { // cookies window options
-    backgroundColor: 'your background color', // ('' == 'rgb(255, 183, 0);')
-    height: 'your height', // ('' == 80px)
-    text: 'your text', // ('' == 'Я даю согласие на использование cookies')
-    textColor: 'your text color', // ('' == 'black')
-    link: 'your link cookie policy', // ('' == 'https://2gdpr.com/cookies')
-    linkColor: 'your text link color', // ('' == '#fff')
-    btnBorder: 'your button border color', // ('' == 'black')
-    btnTextColor: 'your button text color', // ('' == '#fff')
-    cancel: 'your text cancel', // ('' == 'Accept')
-    confirm: 'your text confirm' // ('' == 'Cancel')
-  },
-  { // technical specifications cookies
-    nameConsent: 'your name consent cookies', // ('' == 'site_consent')
-    value: 'your value', // ('' == 'true')
-    expires: 'your expires date' // ('' == 'Session')
-  }
-);
+  {
+    background: 'your background color',        // (default: rgb(255, 183, 0);)
+    height: 'your height',                      // (default: 80px)
+    text: 'your text agreement',                // (default: Я даю согласие на использование cookies)
+    textColor: 'your text color',               // (default: black)
+    link: 'your link cookie policy',            // (default: https://2gdpr.com/cookies)
+    linkColor: 'your text link color',          // (default: #fff)
+    btnBorder: 'your button border color',      // (default: black)
+    btnTextColor: 'your button text color',     // (default: #fff)
+    cancel: 'your text cancel',                 // (default: Accept)
+    confirm: 'your text confirm'                // (default: Cancel)
+    nameConsent: 'your name consent cookies',   // (default: site_consent)
+    valueCookies: 'your value',                 // (default: true)
+    expires: 'your expires date'                // (default: Session) => example: Sun, 16 Jul 3567 06:23:41 GMT
+  });
+
 ```
 
-4. Import your scripts into the “cookieConsent.js” file and write their calls to the myScripts() function:
+4. Write your code in the myScripts() function inside the "cookieConsent.js" file:
+
 ```
 function myScripts() {
   //your scripts();
 }
+
+```
+
+RU: 
+
+Этот модуль предназначен для внедрения файлов cookie на веб-сайте.
+
+Модуль реализован на нативном JavaScript с использованием стандарта ES6.
+
+ИСПОЛЬЗОВАНИЕ:
+
+1. Переместите файл "cookieConsent.js" в папку вашего проекта.
+
+2. Импортируйте с помощью команды "import cookieConsent from ..." в ваш основной файл "main.js".
+
+3. В главном файле вызовите функцию cookieConsent({}), внутри которой в качестве параметра передайте объекты со следующими свойствами:
+
+```
+cookiesConsent(
+  {
+    background: 'your background color',        // (default: rgb(255, 183, 0);)
+    height: 'your height',                      // (default: 80px)
+    text: 'your text agreement',                // (default: Я даю согласие на использование cookies)
+    textColor: 'your text color',               // (default: black)
+    link: 'your link cookie policy',            // (default: https://2gdpr.com/cookies)
+    linkColor: 'your text link color',          // (default: #fff)
+    btnBorder: 'your button border color',      // (default: black)
+    btnTextColor: 'your button text color',     // (default: #fff)
+    cancel: 'your text cancel',                 // (default: Accept)
+    confirm: 'your text confirm'                // (default: Cancel)
+    nameConsent: 'your name consent cookies',   // (default: site_consent)
+    valueCookies: 'your value',                 // (default: true)
+    expires: 'your expires date'                // (default: Session) => example: Sun, 16 Jul 3567 06:23:41 GMT
+  });
+
+```
+
+4. Напишите свой код в функции myScripts() внутри файла "cookieConsent.js":
+
+```
+function myScripts() {
+  //your scripts();
+}
+
 ```
--- cookieConsent.js
+++ cookieConsent.js
@@ -1,165 +1,157 @@
 'use strict';
+export default class CookieConsent {
+	constructor({
+		height = '80px', 
+		background = 'rgb(255, 183, 0)', 
+		link = 'https://2gdpr.com/cookies', 
+		linkColor = '#fff', 
+		text = 'Я даю согласие на использование cookies', 
+		textColor = 'black', 
+		btnBorder = 'black', 
+		btnTextColor = '#fff', 
+		confirm = 'Accept', 
+		cancel = 'Cancel', 
+		nameConsent = 'site_consent', 
+		valueCookies = 'true', 
+		expires
+		} = {}) {
+		this.popup = 'popup';
+		this.btnConfirm = 'data-confirm';
+		this.btnCancel = 'data-cancel';
+		this.consentPropertyType = nameConsent;
+		this.valueCookies = valueCookies;
+		this.expiresCookies = expires;
+		this.linkConsentCookies = link;
+		this.textConsent = text;
+		this.linkColor = linkColor;
+		this.textConsentColor = textColor;
+		this.heightblock = height;
+		this.background = background;
+		this.borderBtnColor = btnBorder;
+		this.btnTextColor = btnTextColor;
+		this.btnConfirmText = confirm;
+		this.btnCancelText = cancel;
+	}
 
-// import your scripts
+	// создание блока Cookies
+	create = () => {
+		const windowCookies = document.createElement('div');
+		windowCookies.classList.add(this.popup);
+		windowCookies.style.cssText = `
+			position: fixed;
+			bottom: -100%;
+			width: 100%;
+			height: ${this.heightblock};
+			background-color: ${this.background};
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			color: ${this.textConsentColor};
+			transition: 0.5s all;
+		`;
+			
+		windowCookies.innerHTML = `
+			<div class="wrapperCookie">
+				${this.textConsent}
+				<button ${this.btnConfirm} class="btn_cookies">${this.btnConfirmText}</button>
+				<button ${this.btnCancel} class="btn_cookies">${this.btnCancelText}</button>
+				<a class="link_cookies" href="${this.linkConsentCookies}">Подробнее</a>
+			</div>
+		`;
 
-function cookiesConsent({backgroundColor, height, text, textColor, link, linkColor, btnBorder, btnTextColor, cancel, confirm}, {nameConsent, value, expires}) {
-	class CookieConsent {
-		constructor(
-			{heightblock, background, linkConsentCookies, linkColor, textConsent, textConsentColor, borderBtnColor, 
-				btnTextColor, btnConfirmText, btnCancelText, consentPropertyType, valueCookies, expiresCookies}) {
-			this.popup = 'popup';
-			this.btnConfirm = 'data-confirm';
-			this.btnCancel = 'data-cancel';
-			this.consentPropertyType = consentPropertyType === '' ? 'site_consent' : consentPropertyType;
-			this.valueCookies = valueCookies === '' ? 'true' : valueCookies;
-			this.expiresCookies = expiresCookies;
-			this.linkConsentCookies = linkConsentCookies === '' ? 'https://2gdpr.com/cookies' : linkConsentCookies;
-			this.textConsent = textConsent === '' ? 'Я даю согласие на использование cookies' : textConsent;
-			this.linkColor = linkColor === '' ? '#fff' : linkColor;
-			this.textConsentColor = textConsentColor === '' ? 'black' : textConsentColor;
-			this.heightblock = heightblock === '' ? '80px' : heightblock;
-			this.background = background === '' ? 'rgb(255, 183, 0)' : background;
-			this.borderBtnColor = borderBtnColor === '' ? 'black' : borderBtnColor;
-			this.btnTextColor = btnTextColor === '' ? '#fff' : btnTextColor;
-			this.btnConfirmText = btnConfirmText === '' ? 'Accept' : btnConfirmText;
-			this.btnCancelText = btnCancelText === '' ? 'Cancel' : btnCancelText;
+		document.querySelector('body').append(windowCookies);
+	};
+
+	// получение ключа из Cookies
+	getItem = (key) => {
+		const cookies = document.cookie.split(';')
+			.map(cookie => cookie.split('='))
+			.reduce((acc, [key, value]) => ({...acc, [key.trim()]: value}), {});
+		return cookies[key];
+	};
+
+	// запись ключа и значения в Cookies
+	setItem = (key, value) => {
+		document.cookie = `${key}=${value}; expires = ${this.expiresCookies}`;
+	};
+
+	// проверка наличия записи ключа в Cookies
+	hasConsented = () => this.getItem(this.consentPropertyType) === `${this.valueCookies}` ? true : false;
+
+	// метод для смены статуса согласия пользователя и загрузки метрик
+	changeStatus = (prop) => {
+		this.setItem(this.consentPropertyType, prop);
+		
+		if(this.hasConsented()) {
+			myScripts(); // загрузка метрик
 		}
+	};
 
-		// создание блока Cookies
-		create = () => {
-			const windowCookies = document.createElement('div');
-			windowCookies.classList.add(this.popup);
-			windowCookies.style.cssText = `
-				position: fixed;
-				bottom: -100%;
-				width: 100%;
-				height: ${this.heightblock};
-				background-color: ${this.background};
-				display: flex;
-				justify-content: center;
-				align-items: center;
-				color: ${this.textConsentColor};
-				transition: 0.5s all;
-			`;
-				
-			windowCookies.innerHTML = `
-				<div class="wrapperCookie">
-					${this.textConsent}
-					<button ${this.btnConfirm} class="btn_cookies">${this.btnConfirmText}</button>
-					<button ${this.btnCancel} class="btn_cookies">${this.btnCancelText}</button>
-					<a class="link_cookies" href="${this.linkConsentCookies}">Подробнее</a>
-				</div>
-			`;
+	// обработчики на кнопки для смены статуса
+	bindTriggers = (popup, btn) => {
+		btn.forEach(el => {
+			el.addEventListener('click', () => {
+				if (el.getAttribute(this.btnConfirm) == '') {
+					this.changeStatus(this.valueCookies);
+					popup.style.bottom = '-100%';
+				}
+				if (el.getAttribute(this.btnCancel) == '') {
+					this.changeStatus(false);
+					popup.style.bottom = '-100%';
+				}
+			});  
+		});
+	};
 
-			document.querySelector('body').append(windowCookies);
-		};
-
-		// получение ключа из Cookies
-		getItem = (key) => {
-			const cookies = document.cookie.split(';')
-				.map(cookie => cookie.split('='))
-				.reduce((acc, [key, value]) => ({...acc, [key.trim()]: value}), {});
-			return cookies[key];
-		};
-
-		// запись ключа и значения в Cookies
-		setItem = (key, value) => {
-			document.cookie = `${key}=${value}; expires = ${this.expiresCookies}`;
-		};
-
-		// проверка наличия записи ключа в Cookies
-		hasConsented = () => this.getItem(this.consentPropertyType) === `${this.valueCookies}` ? true : false;
-
-		// метод для смены статуса согласия пользователя и загрузки метрик
-		changeStatus = (prop) => {
-			this.setItem(this.consentPropertyType, prop);
-			
-			if(this.hasConsented()) {
-				myScripts(); // загрузка метрик
-			}
-		};
-
-		// обработчики на кнопки для смены статуса
-		bindTriggers = (popup, btn) => {
-			btn.forEach(el => {
-				el.addEventListener('click', () => {
-					if (el.getAttribute(this.btnConfirm) == '') {
-						this.changeStatus(this.valueCookies);
-						popup.style.bottom = '-100%';
-					}
-					if (el.getAttribute(this.btnCancel) == '') {
-						this.changeStatus(false);
-						popup.style.bottom = '-100%';
-					}
-				});  
-			});
-		};
-
-		// инициализация cookie
-		init = () => {
-			this.create();
-			const popup = document.querySelector(`.${this.popup}`),
-				  btnCookies = document.querySelectorAll('.btn_cookies'),
-				  linkCookies = document.querySelector('.link_cookies');
-			
-			btnCookies.forEach(btn => {
-				btn.style.cssText = `
-					width: 150px;
-					height: 50px;
-					text-align: center;
-					color: ${this.btnTextColor};
-					margin-left: 30px;
-					border: 1px solid ${this.borderBtnColor};
-					background-color: transparent;
-				`;
-			});
-
-			linkCookies.style.cssText = `
-				margin-left: 30px;
+	// инициализация cookie
+	render = () => {
+		this.create();
+		const popup = document.querySelector(`.${this.popup}`),
+				btnCookies = document.querySelectorAll('.btn_cookies'),
+				linkCookies = document.querySelector('.link_cookies');
+		
+		btnCookies.forEach(btn => {
+			btn.style.cssText = `
 				width: 150px;
 				height: 50px;
 				text-align: center;
-				color: ${this.linkColor};
+				color: ${this.btnTextColor};
+				margin-left: 30px;
+				border: 1px solid ${this.borderBtnColor};
 				background-color: transparent;
 			`;
-			
-			try {
-				if(this.hasConsented()) {
-					myScripts();
-				} else {
-					popup.style.bottom = 0;
-				}
-				this.bindTriggers(popup, btnCookies);
-				
-			} catch(e) {
-				console.error(e);
+		});
+
+		linkCookies.style.cssText = `
+			margin-left: 30px;
+			width: 150px;
+			height: 50px;
+			text-align: center;
+			color: ${this.linkColor};
+			background-color: transparent;
+		`;
+		
+		try {
+			if(this.hasConsented()) {
+				myScripts();
+			} else {
+				popup.style.bottom = 0;
 			}
-		};
-
-	}
-
-	new CookieConsent({
-		consentPropertyType: nameConsent,
-		valueCookies: value,
-		expiresCookies: expires,
-		linkConsentCookies: link,
-		textConsent: text,
-		linkColor: linkColor,
-		textConsentColor: textColor,
-		heightblock: height,
-		background: backgroundColor,
-		borderBtnColor: btnBorder,
-		btnTextColor: btnTextColor,
-		btnConfirmText: confirm, 
-		btnCancelText: cancel
-	}).init();
-
-	function myScripts() {
-		//your scripts
-	}
+			this.bindTriggers(popup, btnCookies);
+			
+		} catch(e) {
+			console.error(e);
+		}
+	};
 }
 
-export default cookiesConsent;
+	
+
+function myScripts() {
+	console.log('Hello World');
+	//your script
+}
+
 
 
 
